9d69bd741388e86225b41fe20a506c9b88d53efd
Liz <liz@xerb.tv>
2018-07-25 11:26:53 -0600

    db fix

--- db.py
+++ db.py
@@ -1,13 +1,12 @@
 import sqlite3
-
 import click
-from flask import current_app, g
+from flask import current_app, g, Flask
 from flask.cli import with_appcontext
 
 def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(
-                current_app.config['DATABASE'],
+                'db.sqlite',
                 detect_types=sqlite3.PARSE_DECLTYPES
             )
         g.db.row_factory = sqlite3.Row
